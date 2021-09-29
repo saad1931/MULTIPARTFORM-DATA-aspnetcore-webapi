18828c458014a8b87c8509115825f13af22414d6
Muhammad Saad <muhammadsaad3119@gmail.com>
2021-09-29 23:42:17 +0500

    Add project files.

---
+++ MULTIPARTFORM-DATA/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MULTIPARTFORM_DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MULTIPARTFORM_DATA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Student([FromForm] StudentModel std)
+        {
+            //Getting Name
+            string name = std.Name;
+            // Getting Image
+            var image = std.Image;
+
+            var filePath = Path.Combine("wwwroot/images", image.FileName);
+            // Saving Image on www/root
+            if (image.Length > 0)
+            {
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    image.CopyTo(fileStream);
+                }
+            }
+            
+
+            return Ok(new { status = true, message = "Student Posted Successfully" });
+        }
+    }
+}
---
+++ MULTIPARTFORM-DATA/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MULTIPARTFORM_DATA.Models
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
